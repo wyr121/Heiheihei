cde772e35c7ecfc10ca1dc0515b0061ea498e39b
wyr121 <wuyinrui03@gmail.com>
2024-12-13 18:09:27 +0800

    修复玩家面向的bug

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -105,7 +105,7 @@ public class Player : MonoBehaviour, IKitchenObjectParent
         if (!canMove)
         {
             var moveDirX = new Vector3(moveDir.x, 0, 0).normalized;
-            canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirX, moveDistance);
+            canMove = moveDir.x != 0 && !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirX, moveDistance);
             if (canMove)
             {
                 moveDir = moveDirX;
@@ -115,7 +115,7 @@ public class Player : MonoBehaviour, IKitchenObjectParent
                 if (!canMove)
                 {
                     var moveDirZ = new Vector3(0, 0, moveDir.z).normalized;
-                    canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirZ, moveDistance);
+                    canMove = moveDir.z != 0 && !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirZ, moveDistance);
                     if (canMove)
                     {
                         moveDir = moveDirZ;
